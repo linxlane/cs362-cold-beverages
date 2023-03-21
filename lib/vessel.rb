001acf5a39075c89df4dc504dbb340d941df314c
Linnea Lane <linnealane@gmail.com>
2023-03-20 19:36:43 -0700

    modified vessel class to pass second spec test

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,13 @@
 class Vessel
   attr_reader :name, :volume
 
-  def initialize(name = 'FAKE', volume = 100)
+  def initialize(name = 'FAKE', volume = 100, contents = [])
     @name = name
     @volume = volume
+    @contents = contents
   end
 
+  def empty?
+    @contents == []
+  end
 end
