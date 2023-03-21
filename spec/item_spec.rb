8ec0455465594c4f40daa1c42ce04bcb74c39923
Linnea Lane <linnealane@gmail.com>
2023-03-21 00:22:59 -0700

    added initialization test in item_spec

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,9 @@
 require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
-
+  it 'can be initialized' do
+    item = Item.new('thing', 100)
+    expect(item.name).to eq('thing')
+    expect(item.volume).to eq(100)
+  end
 end
