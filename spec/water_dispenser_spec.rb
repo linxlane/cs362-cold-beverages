0c30a0ade4fafafd505f7d0eea05e915a97c01a5
Linnea Lane <linnealane@gmail.com>
2023-03-21 00:26:07 -0700

    added initialization test in water_dispenser_spec

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,5 +1,9 @@
 require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
-
+it 'can be initialized' do
+    waterReservior = WaterReservoir.new
+    waterDispenser = WaterDispenser.new(waterReservior)
+    expect(waterDispenser).to respond_to(:reservoir)
+  end
 end
