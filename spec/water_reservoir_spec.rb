f8c7ee40ae4fe0780f3f39256c4c0b864d8853d7
Linnea Lane <linnealane@gmail.com>
2023-03-21 12:52:59 -0700

    implemented initialization test in water_reservoir_spec

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
-
+ it 'can be initialized with default values' do
+  reservoir = WaterReservoir.new
+  expect(reservoir).to be_an_instance_of(WaterReservoir)
+  expect(reservoir.capacity).to eq(10)
+  expect(reservoir.current_water_volume).to eq(0)
+ end
 end
