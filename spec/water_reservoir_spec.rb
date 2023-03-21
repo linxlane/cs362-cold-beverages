334544bd99fb8945c9fb1f3c939e5e8d1dc0c3e5
Linnea Lane <linnealane@gmail.com>
2023-03-21 12:55:55 -0700

    implemented empty? test in water_reservoir_spec

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -7,4 +7,12 @@ describe 'A water reservoir' do
   expect(reservoir.capacity).to eq(10)
   expect(reservoir.current_water_volume).to eq(0)
  end
+
+ it 'can check if it is empty' do
+  reservoirOne = WaterReservoir.new
+  reservoirTwo = WaterReservoir.new(10, 5)
+
+  expect(reservoirOne.empty?).to be_truthy
+  expect(reservoirTwo.empty?).to be_falsey
+ end
 end
