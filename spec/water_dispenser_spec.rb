67f86c4a0826953c58b673b63c609f8f15896814
Linnea Lane <linnealane@gmail.com>
2023-03-21 12:36:08 -0700

    implemented dispense test in water_dispenser_spec

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,9 +1,19 @@
 require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
-it 'can be initialized' do
+  it 'can be initialized' do
     waterReservior = WaterReservoir.new
     waterDispenser = WaterDispenser.new(waterReservior)
     expect(waterDispenser).to respond_to(:reservoir)
+  end
+
+  it 'can dispense' do
+    vessel = Vessel.new('cup', 20)
+    reservoir = WaterReservoir.new(100, 100)
+    dispenser = WaterDispenser.new(reservoir)
+
+    dispenser.dispense(vessel)
+
+    expect(reservoir.current_water_volume).to eq(100-20)
   end
 end
