c024bd59f79724dd38e79f3b944066eeabb450c4
Linnea Lane <linnealane@gmail.com>
2023-03-21 19:41:53 -0700

    implemented freezer method test in refrigerator_spec

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -22,4 +22,16 @@ describe 'A refrigerator' do
     fridge.chill(item)
     expect(chiller.remaining_capacity).to eq(90)
   end
+
+  it 'can freeze items' do
+    item = Item.new('popsicle', 5)
+    chiller = Chiller.new
+    freezer = Freezer.new
+    waterReservoir = WaterReservoir.new
+    waterDispenser = WaterDispenser.new(waterReservoir)
+    fridge = Refrigerator.new(chiller, freezer, waterDispenser, waterReservoir)
+
+    fridge.freeze(item)
+    expect(freezer.remaining_capacity).to eq(95)
+  end
 end
