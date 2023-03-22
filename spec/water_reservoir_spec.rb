c4ab4c98d71cf985978c176c29a776f9e4065054
Linnea Lane <linnealane@gmail.com>
2023-03-21 19:05:20 -0700

    implemented fill method test in water_reservoir_spec

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -1,18 +1,23 @@
 require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
- it 'can be initialized with default values' do
-  reservoir = WaterReservoir.new
-  expect(reservoir).to be_an_instance_of(WaterReservoir)
-  expect(reservoir.capacity).to eq(10)
-  expect(reservoir.current_water_volume).to eq(0)
- end
+    it 'can be initialized with default values' do
+    reservoir = WaterReservoir.new
+    expect(reservoir).to be_an_instance_of(WaterReservoir)
+    expect(reservoir.capacity).to eq(10)
+    expect(reservoir.current_water_volume).to eq(0)
+  end
 
- it 'can check if it is empty' do
-  reservoirOne = WaterReservoir.new
-  reservoirTwo = WaterReservoir.new(10, 5)
+  it 'can check if it is empty' do
+    reservoirOne = WaterReservoir.new
+    reservoirTwo = WaterReservoir.new(10, 5)
 
-  expect(reservoirOne.empty?).to be_truthy
-  expect(reservoirTwo.empty?).to be_falsey
- end
+    expect(reservoirOne.empty?).to be_truthy
+    expect(reservoirTwo.empty?).to be_falsey
+  end
+
+  it 'can be filled' do 
+    reservoir = WaterReservoir.new
+    expect(reservoir.fill).to eq(10)
+  end
 end
