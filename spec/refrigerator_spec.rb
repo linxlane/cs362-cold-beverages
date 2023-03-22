e1b02c3d25a3ae5dc20a20248b416a52b6c8a336
Linnea Lane <linnealane@gmail.com>
2023-03-21 19:37:15 -0700

    implemented refrigerator initialization test in refrigerator_spec

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -1,5 +1,13 @@
 require_relative '../lib/refrigerator'
 
 describe 'A refrigerator' do
-
+  it 'can be initialized' do
+    chiller = Chiller.new
+    freezer = Freezer.new
+    waterReservoir = WaterReservoir.new
+    waterDispenser = WaterDispenser.new(waterReservoir)
+    fridge = Refrigerator.new(chiller, freezer, waterDispenser, waterReservoir)
+    
+    expect(fridge).to be_a_kind_of(Refrigerator)
+  end
 end
