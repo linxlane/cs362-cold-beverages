34b8f8010a4214c3eaa1f7144c5ed330f9917be3
Linnea Lane <linnealane@gmail.com>
2023-03-21 19:10:58 -0700

    implemented initialization test in freezer_spec

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -1,5 +1,11 @@
 require_relative '../lib/freezer'
 
 describe 'A freezer' do
+  it 'can be initialized' do
+    freezer = Freezer.new
 
+    expect(freezer).to be_a_kind_of(Freezer)
+    expect(freezer.capacity).to eq(100)
+    expect(freezer.temperature).to eq(70)
+  end
 end
